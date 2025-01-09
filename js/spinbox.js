8db82e820c3b6b1af2e4e7f8b3a7566983eeb7ba
leafpad <155798178+codingleaf@users.noreply.github.com>
2025-01-09 22:44:23 +0800

    fix: add hold increment and decrement for mobile

--- js/spinbox.js
+++ js/spinbox.js
@@ -6,42 +6,27 @@ export class Spinbox {
         this.input = this.spinbox.querySelector('.spinbox-input');
         this.incrementButton = this.spinbox.querySelector('.spinbox-increment');
         this.decrementButton = this.spinbox.querySelector('.spinbox-decrement');
-
+        this.intervalID;
         this.init();
     }
   
     init() {
         this.incrementButton.addEventListener('click', () => this.changeValue(1));
         this.decrementButton.addEventListener('click', () => this.changeValue(-1));
- 
-        let intervalId;
 
-        // For holding the button for rapid increment
-        this.incrementButton.addEventListener('mousedown', () => {
-            intervalId = setInterval(() => {
-                this.changeValue(1);  // Increase value by 1
-            }, 100);  // Adjust the speed (in milliseconds)
-        });
+        // Add event listeners for both mouse and touch events
+        this.incrementButton.addEventListener('mousedown', () => this.startChange(1));
+        this.incrementButton.addEventListener('touchstart', () => this.startChange(1));
+        this.decrementButton.addEventListener('mousedown', () => this.startChange(-1));
+        this.decrementButton.addEventListener('touchstart', () => this.startChange(-1));
 
-        // For holding the button for rapid decrement
-        this.decrementButton.addEventListener('mousedown', () => {
-            intervalId = setInterval(() => {
-                this.changeValue(-1);  // Decrease value by 1
-            }, 100);  // Adjust the speed (in milliseconds)
-        });
-
-        // Stop rapid change when the button is released
-        document.addEventListener('mouseup', () => {
-            clearInterval(intervalId);
-        });
-
-        // Stop rapid change if the mouse leaves the button
-        this.incrementButton.addEventListener('mouseleave', () => {
-            clearInterval(intervalId);
-        });
-        this.decrementButton.addEventListener('mouseleave', () => {
-            clearInterval(intervalId);
-        });
+        // Stop the action on mouseup, touchend, or when the user moves away
+        this.incrementButton.addEventListener('mouseup', () => this.stopChange());
+        this.incrementButton.addEventListener('mouseleave', () => this.stopChange());
+        this.incrementButton.addEventListener('touchend', () => this.stopChange());
+        this.decrementButton.addEventListener('mouseup', () => this.stopChange());
+        this.decrementButton.addEventListener('mouseleave', () => this.stopChange());
+        this.decrementButton.addEventListener('touchend', () => this.stopChange());
 
         this.input.addEventListener('input', () => this.validateValue());
 
@@ -51,7 +36,18 @@ export class Spinbox {
             }
         })
     }
-  
+
+    stopChange() {
+        clearInterval(this.intervalID);
+    }
+
+    // Start the increment/decrement action
+    startChange(delta) {
+        this.intervalID = setInterval(() => {
+            this.changeValue(delta);  // Increase value by 1
+        }, 100);  // Adjust the speed (in milliseconds)
+    }
+
     changeValue(delta) {
         const currentValue = parseInt(this.input.value, 10) || 0;
         const minValue = parseInt(this.input.min, 10) || -Infinity;
